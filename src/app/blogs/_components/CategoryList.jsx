86c021c1bfa3fccb91967898b47d11eb5d108c8d
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2025-06-18 11:31:23 +0330

    environment variabled added to development phase and also fetching categorylist api address set to be dynamic baseurl.

--- src/app/blogs/_components/CategoryList.jsx
+++ src/app/blogs/_components/CategoryList.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 async function CategoryList() {
-  const res = await fetch("http://localhost:5000/api/category/list");
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/category/list`);
   const {
     data: { categories },
   } = await res.json();
