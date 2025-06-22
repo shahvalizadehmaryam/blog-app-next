ef22026df0bae2a19ef3561a048f8b8cd903ec73
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2025-06-22 10:02:12 +0330

    streaming with suspense for postlist developed.

---
+++ src/app/blogs/_components/PostList.jsx
@@ -0,0 +1,17 @@
+async function PostList() {
+  await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulating a delay
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post/list`);
+  const {
+    data: { posts },
+  } = await res.json();
+  console.log(posts);
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
+  );
+}
+
+export default PostList;
