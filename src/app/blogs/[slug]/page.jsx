6065f5f6092e15e31e88017a5b345ca982cd2ae6
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2025-07-16 10:23:56 +0330

    posts comment section developed.

--- src/app/blogs/[slug]/page.jsx
+++ src/app/blogs/[slug]/page.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 import React from "react";
 import RelatedPost from "../_components/RelatedPosts";
+import PostComments from "../_components/comment/PostComment";
 
 // if a page except these 6 blog pages doesn't exist it will show 404
 export const dynamicParams = false;
@@ -37,10 +38,12 @@ async function SinglePost({ params }) {
         <Image
           className="object-cover object-center hover:scale-110 transition-all ease-out duration-300"
           fill
+          alt={post.slug}
           src={post.coverImageUrl}
         />
       </div>
       {post.related.length > 0 && <RelatedPost posts={post.related} />}
+      {post.comments.length > 0 && <PostComments post={post} />}
     </div>
   );
 }
