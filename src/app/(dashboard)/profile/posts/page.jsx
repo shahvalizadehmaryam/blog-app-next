d3ea16385003b3829323e4d1f56ef6b42a8c8486
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2025-08-04 11:28:03 +0330

    search post added.

--- src/app/(dashboard)/profile/posts/page.jsx
+++ src/app/(dashboard)/profile/posts/page.jsx
@@ -1,12 +1,21 @@
 import { Suspense } from "react";
 import PostsTable from "./_/components/PostsTable";
 import Spinner from "@/ui/Spinner";
+import Search from "@/ui/Search";
+import { CreatePost } from "./_/components/Buttons";
+import queryString from "query-string";
 
-export default function PostsPage() {
+export default function PostsPage({ searchParams }) {
+  const queries = queryString.stringify(searchParams);
   return (
     <div>
-      <Suspense fallback={<Spinner />}>
-        <PostsTable />
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 text-secondary-700 mb-12 items-center">
+        <h1 className="font-bold text-xl">لیست پست ها</h1>
+        <Search />
+        <CreatePost />
+      </div>
+      <Suspense fallback={<Spinner />} key={queries}>
+        <PostsTable queries={queries} />
       </Suspense>
     </div>
   );
